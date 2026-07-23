e0bae760f2787041468df1e902a37b894b0fec0f
omkarb21 <58302404+omkarb21@users.noreply.github.com>
2026-07-23 14:53:35 -0400

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.cpp
@@ -0,0 +1,28 @@
+using namespace std;
+#include<string>
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+    //     //brute force
+    //     unordered_map<char,int>st1;
+    //     unordered_map<char,int>st2;
+    //     for(char c:s){
+    //         st1[c]++;
+    //     }
+
+    //     for(char c:t){
+    //         st2[c]++;
+    //     }
+
+    //     return st1==st2;
+
+    // }
+
+    //approach 2
+    sort(s.begin(),s.end());
+    sort(t.begin(),t.end());
+
+    return s==t;
+    }
+};
