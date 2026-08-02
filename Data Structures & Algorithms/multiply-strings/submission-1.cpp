7e8f4ac0ac35655d6475ae7e6846d14155b8f8a9
omkarb21 <58302404+omkarb21@users.noreply.github.com>
2026-08-02 13:03:34 -0400

    Add: multiply-strings - submission-1

---
+++ Data Structures & Algorithms/multiply-strings/submission-1.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        int m = num1.size();
+        int n = num2.size();
+        vector<int> resInt(m + n, 0);
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int digitProduct = (num1[i] - '0') * (num2[j] - '0');
+
+                int unitsIdx = i + j + 1;   // less significant slot
+                int carryIdx = i + j;       // one place more significant
+
+                int total = digitProduct + resInt[unitsIdx];
+                resInt[unitsIdx] = total % 10;
+                resInt[carryIdx] += total / 10;
+            }
+        }
+
+        for(int idx=resInt.size()-1;idx>=1;idx--){
+            int carr=resInt[idx]/10;
+            resInt[idx]=resInt[idx]%10;
+            resInt[idx-1]+=carr;
+        }
+
+        string result="";
+        
+        int i=0;
+        for(int num:resInt){
+            if(num==0)i++;
+            else{
+                break;
+            }
+        }
+        if(i==resInt.size()) return "0";
+        while(i<resInt.size()){
+            result+=char('0'+resInt[i]);
+            i++;
+        }
+
+
+
+
+
+        return result;
+        
+    }
+};
