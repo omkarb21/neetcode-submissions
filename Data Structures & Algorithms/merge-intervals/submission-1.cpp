27ee66c674b1170dd4ab6924b78a869c39cc059e
omkarb21 <58302404+omkarb21@users.noreply.github.com>
2026-08-04 10:26:44 -0400

    Add: merge-intervals - submission-1

---
+++ Data Structures & Algorithms/merge-intervals/submission-1.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(), [](vector<int>a,vector<int>b){return a[0]<b[0];});
+
+
+        for(int i=1;i<intervals.size();){
+            if(intervals[i-1][1]>=intervals[i][0]){
+                intervals[i-1][1]=max(intervals[i][1],intervals[i-1][1]);
+                intervals.erase(intervals.begin()+i);
+            }
+            else{
+                i++;
+            }
+        }
+
+        return intervals;
+    }
+};
+
+//class Solution {
+// public:
+//     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+//         sort(intervals.begin(), intervals.end());
+
+//         vector<vector<int>> res;
+//         for (auto& in : intervals) {
+//             if (!res.empty() && res.back()[1] >= in[0]) {
+//                 res.back()[1] = max(res.back()[1], in[1]);
+//             } else {
+//                 res.push_back(in);
+//             }
+//         }
+//         return res;
+//     }
+//};
